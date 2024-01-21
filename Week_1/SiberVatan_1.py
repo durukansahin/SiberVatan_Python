19ce29812a312cb28fed81612598d89b0363fdc7
Durukan Şahin <156955364+durukansahin@users.noreply.github.com>
2024-01-21 17:32:16 +0300

    week1

---
+++ Week_1/SiberVatan_1.py
@@ -0,0 +1,60 @@
+print("H.e.l.l.o  W.o.r.l.d")
+#yorum satırı açmak için '#' isareti yada """ yorum satırı""" böylede yapabiliriz
+"""
+ertf
+ret
+ertf
+drtger
+"""
+
+siber = 14  
+sayi=10
+sayi=100
+#son değer 100 olduğu için artık 100 olarak kabul edilir
+print(sayi)
+
+yas = 10
+YAS = 100
+#ikisi farklı
+print(yas)
+
+print(2**4)#2 üssü 4 olarak olur ** üs alır
+print(2*4)#2*8 olarak çarpar
+
+adi,yas,sehir = ("ali",15,"Bolu")
+
+kelime = "merhaba\ndunya "
+print(kelime) # satır atlayark merhaba dünya yazar
+
+x,y,z,=(4,5,6)
+
+sayilar=("10")
+sayilar1=("20")
+print(sayilar+sayilar1) # sonuç=1020
+
+maas=17002
+vergi=0.20
+print(17002- (17002*0.20))#amatör
+
+print(maas - (maas*vergi))#usta
+
+isim="Durukan"
+print("merhaba," + isim + " hoşgeldin")#çıktı merhaba, Durukan hoşeldin
+
+sayi= -7
+print("mutlak değer", abs(sayi))#negatif sayınını mutlak değerini almamıza yardımcı olur
+
+us= pow(2,3)#üs alma sonuç=8
+print("sayiüssü=",us)
+
+from math import sqrt,ceil,floor # kütüphaneden çağırdık
+sayi = 25
+karekok = sqrt(sayi)
+print("sayinin karekökü=", karekok)#sayıların karekok almamıza yarar 
+
+ondalik = 10.25
+yukari= ceil(ondalik)
+asagi= floor(ondalik)
+print("yukarı=", yukari)
+print("aşağı=", asagi)
+
